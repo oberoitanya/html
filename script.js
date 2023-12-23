a5f9140c88fc5f22373ea759a5050ba961802c75
oberoitanya <147085217+oberoitanya@users.noreply.github.com>
2023-12-24 01:52:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// This is a comment in JavaScript
+
+// Function to display a greeting
+function greet() {
+    var name = prompt("What's your name?");
+    if (name !== null && name !== "") {
+      alert("Hello, " + name + "!");
+    }
+  }
+  
+  // Function to change the background color
+  function changeColor(color) {
+    document.body.style.backgroundColor = color;
+  }
+  
