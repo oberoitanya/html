5ca61ff4163db81148dedf530a41a4065a64e509
oberoitanya <147085217+oberoitanya@users.noreply.github.com>
2023-12-24 01:59:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// This is a comment in JavaScript
 
-// Function to display a greeting
 function greet() {
     var name = prompt("What's your name?");
     if (name !== null && name !== "") {
@@ -11,5 +9,11 @@ function greet() {
   // Function to change the background color
   function changeColor(color) {
     document.body.style.backgroundColor = color;
+  }
+  
+  // Function to toggle a class on an element
+  function toggleClass() {
+    var element = document.getElementById("toggleElement");
+    element.classList.toggle("highlight");
   }
   
